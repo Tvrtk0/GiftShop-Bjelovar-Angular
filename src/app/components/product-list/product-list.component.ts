a0e009edb74038420ae6779aa97ea4fa33bb15a6
Tvrtk0 <tvrtkobabic2@gmail.com>
2021-10-02 13:56:26 +0200

    Add filtering by category id

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,16 +11,32 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   // assign results from getProductList Observable to the products array
   listProducts() {
-    this.productService.getProductList().subscribe(
+
+    // check if "id" parametar is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    // get "id" and convert it to nmbr
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
