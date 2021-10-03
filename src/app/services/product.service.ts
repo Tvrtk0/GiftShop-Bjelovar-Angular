4dcf7c37b91fa448d97214a2239bab8b8cf77e65
Tvrtk0 <tvrtkobabic2@gmail.com>
2021-10-03 13:35:49 +0200

    Fix typo

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -30,7 +30,7 @@ export class ProductService {
   getProductCategories(): Observable<ProductCategory[]> {
     
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
-      map(response => response._embedded.products)
+      map(response => response._embedded.productCategory)
     )
   }
 }
@@ -44,6 +44,6 @@ interface GetResponseProducts {
 
 interface GetResponseProductCategory {
   _embedded: {
-    products: ProductCategory[];
+    productCategory: ProductCategory[];
   }
 }
