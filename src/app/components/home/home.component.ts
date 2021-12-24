a915a49bfc754d1456b876178ce1678e418e0c20
Tvrtk0 <tvrtkobabic2@gmail.com>
2021-12-24 23:04:04 +0100

    create Novo u ponudi

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductsByDate().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
   }
 
 }
