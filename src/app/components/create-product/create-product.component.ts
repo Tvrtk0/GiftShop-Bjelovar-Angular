c7f87c282c9d3110ea4d518a5ee1019d34a04bbb
Tvrtk0 <tvrtkobabic2@gmail.com>
2022-01-05 22:53:22 +0100

    fix create product form

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -33,33 +33,32 @@ export class CreateProductComponent implements OnInit {
     );
     
     this.productForm = this.formBuilder.group( {
-      'name': new FormControl('', [Validators.required, Validators.minLength(2)]),
       'productCategory': new FormControl('', [Validators.required]),
+      'price': new FormControl('', [Validators.required, Validators.pattern('^(?!-)[0-9]*(\.[0-9]{0,2})?$')]),
+      'name': new FormControl('', [Validators.required, Validators.minLength(2)]),
       'description': new FormControl('', [Validators.required, Validators.minLength(10)]),
-      'price': new FormControl('', [Validators.required, Validators.pattern('^[0-9]*(\.[0-9]{0,2})?$')]),
+      'archive': new FormControl(false),
     });
-    // provjerit regex
-    //'imageInput' : new FormControl('', [Validators.required])
   }
 
   onSubmit() {
+    let formObject = this.productForm.value;
     this.product.id = null;
-    this.product.name = this.productForm.value.name;
-
+    this.product.name = formObject.name;
+    this.product.price = formObject.price;
+    this.product.description = formObject.description;
+    this.product.archive = formObject.archive;
+    this.product.imageUrl = this.imgUrl;
 
     // Store current image object -> default is delete after 24h
-    // this.currentUuid
+    this.productService.storeImage(this.currentUuid).subscribe();
 
+    this.productService.addProduct(this.product, formObject.productCategory).subscribe();
   }
 
   onUpload(info: any) {
     this.message = info.original_filename;
     this.currentUuid = info.uuid;
     this.imgUrl = info.cdnUrl;
-  }
-
-  onChange(file: any) {
-    console.log('fired Event "onChange with data:"');
-    console.log(file);
   }
 }
