3c1e85a1d74e9ceaff4c7cbff7fa0e4efc01c8b3
Tvrtk0 <tvrtkobabic2@gmail.com>
2022-01-07 21:52:43 +0100

    finish edit-product-category

--- src/app/components/edit-product-category/edit-product-category.component.ts
+++ src/app/components/edit-product-category/edit-product-category.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-edit-product-category',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductCategoryComponent implements OnInit {
 
-  constructor() { }
+  categoryForm : FormGroup;
+  category : ProductCategory = new ProductCategory();
+  currentCategoryId: number = null;
+  message: string = null;
+
+  constructor(private formBuilder: FormBuilder,
+              private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.categoryForm = this.formBuilder.group( {
+      'name': new FormControl('', [Validators.required, Validators.minLength(2)])
+    });
+
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('idCat');
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('idCat');
+      
+      this.productService.getProductCategory(this.currentCategoryId).subscribe(
+        (data) => {
+          this.categoryForm.controls['name'].setValue(`${data.categoryName}`);
+        });
+    }
   }
 
+  onSubmit() {
+    this.category.id = this.currentCategoryId;
+    this.category.categoryName = this.categoryForm.value.name;
+    this.message = "Uspješeno spremljeno";
+
+    this.productService.updateProductCategory(this.category)
+      .subscribe(res => {
+
+        console.log(res);
+      });
+  }
 }
