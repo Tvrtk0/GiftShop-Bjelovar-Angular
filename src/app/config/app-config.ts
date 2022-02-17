2862bd7c3ddc86430a18398ade92e29ae141dfb1
Tvrtk0 <tvrtkobabic2@gmail.com>
2022-02-17 13:08:23 +0100

    okta redirect uri update

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa2l2kyv1porSfA05d7',
         issuer: 'https://dev-06452174.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'http://giftshopbjelovar.com/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
